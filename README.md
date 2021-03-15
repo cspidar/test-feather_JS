bda2497c54f3107e9d4c25d3814ba824332244de
cspidar <32433348+cspidar@users.noreply.github.com>
2021-03-15 18:06:30 +0900

    1

---
+++ README.md
@@ -0,0 +1,10 @@
+# Feathers Webpack and React
+This is a simple boilerplate to help you get started with Feathers Webpack and React. Hot reload is enabled. Start the project with:
+
+```
+npm start
+```
+
+The application will be served at ```http://localhost:3030```.
+
+Contributions and/or questions are always welcome.
---
+++ public/ascii_test.js
@@ -0,0 +1,14 @@
+// const Asciidoctor = require('asciidoctor')
+
+const asciidoctor = Asciidoctor()
+
+
+
+
+
+// var doc = asciidoctor.convertFile('./Asciidoctor_test/03_body.adoc', { to_file: false, standalone: true })
+
+var html = asciidoctor.convert('Hello, _Asciidoctor_')
+console.log(html)
+
+document.getElementById("target_ascii").innerHTML=html;
